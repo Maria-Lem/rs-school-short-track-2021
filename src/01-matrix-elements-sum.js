a99d785e51c25106fcd1fbc0fed7cc6a9acc60f8
Maria <maria131621@yandex.ru>
2021-04-07 20:50:09 +0300

    feat: add common character count solution

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,16 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  // throw new Error('Not implemented');
+  // let elem;
+  return matrix.map((elem, i) => elem[i] + elem[i + 1]);
+  // let item = 0;
+  // for (let i = 0; i < matrix.length; i++) {
+  //   item += matrix[i];
+  //   console.log(item);
+  // }
+  // return item;
 }
 
 module.exports = getMatrixElementsSum;
--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,22 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const s1Array = s1.split('');
+  const s2Array = s2.split('');
+  let index = 0;
+  let count = 0;
+
+  s1Array.filter((str1) => {
+    index = s2Array.findIndex((str2) => str1 === str2);
+    if (index >= 0) {
+      count++;
+      s2Array.splice(index, 1);
+    }
+    return count;
+  });
+
+  return count;
 }
 
 module.exports = getCommonCharacterCount;
