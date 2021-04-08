b6255608712480ef7e555f31f8dba8c3968384bd
Maria <maria131621@yandex.ru>
2021-04-08 16:32:26 +0300

    feat: add delete digit solution

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,27 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let result = 0;
+  const digits = [];
+  let num = n;
+
+  while (num) {
+    digits.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+
+  for (let i = 0; i < digits.length; i++) {
+    let elem = 0;
+    for (let j = digits.length - 1; j >= 0; j--) {
+      if (j !== i) {
+        elem = elem * 10 + digits[j];
+      }
+    }
+    result = Math.max(elem, result);
+  }
+
+  return result;
 }
 
 module.exports = deleteDigit;
